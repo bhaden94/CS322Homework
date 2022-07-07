368b68f2e965b82ae94a2040c69fc5552ec65304
bhaden94 <bhaden94@yahoo.com>
2022-07-07 19:16:35 -0400

    Start HW5 database work.

---
+++ Scripts/sql/dbo.category.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[category] (
+    [Id]          INT           NOT NULL,
+    [name]        VARCHAR (30)  NULL,
+    [description] VARCHAR (250) NULL,
+    [image]       IMAGE         NULL,
+    [thumbnail]   IMAGE         NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Scripts/sql/dbo.products.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[products] (
+    [Id]          INT          NOT NULL,
+    [productName] VARCHAR (50) NOT NULL,
+    [categoryId] INT NOT NULL, 
+    [categoryName] VARCHAR(30) NOT NULL, 
+    [description] VARCHAR(250) NOT NULL, 
+    [inventoryAmount] INT CHECK ([inventoryAmount] >= 0) NOT NULL, 
+    [cost] NUMERIC NOT NULL, 
+    [price] MONEY NOT NULL, 
+    [weight] FLOAT NOT NULL, 
+    [image] IMAGE NOT NULL, 
+    [thumbnail] IMAGE NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC) 
+);
+
+
+
